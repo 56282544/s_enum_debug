24f00261152afa7ac494d3f37dd02f7b0bf5f726
淦汤 刘 <15116818275@139.com>
2017-09-09 16:56:53 +0800

    添加项目文件。

---
+++ s_enum_debug/s_enum_debug.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+char * s_gets(char * st, int n);
+
+char *s_gets(char *st, int n) {
+	char * ret_val;
+	char * find;
+	ret_val = fgets(st, n, stdin);
+	if (ret_val)
+	{
+		find = strchr(st, '\n');
+		if (find)
+		{
+			*find = '\0';
+		}
+		else
+		{
+			while (getchar() != '\n')
+			{
+				continue;
+			}
+		}
+	}
+
+	return ret_val;
+}
+
+enum spectrum { red, orange, yellow, green, blue, violet };
+const char * colors[] = { "red", "orange", "yellow", "green", "blue", "violet" };
+#define LEN 30
+
+int main() {
+	char choice[LEN];
+	enum spectrum color;
+	bool color_is_found = false;
+
+	puts("Enter a color (empty line to quit): ");
+	while (s_gets(choice, LEN) != NULL && choice[0] != '\0')
+	{
+		for (color = red; color <= violet; color++) {
+			if (strcmp(choice, colors[color]) == 0)
+			{
+				color_is_found = true;
+				break;
+			}
+		}
+		if (color_is_found)
+		{
+			switch (color) {
+			case red: puts("Rose are red.");
+				break;
+			case orange: puts("Poppies are orange.");
+				break;
+			case yellow: puts("Sunflowers are yellow.");
+				break;
+			case green: puts("Grass id green");
+				break;
+			case blue: puts("Bluebells are blue.");
+				break;
+			case violet: puts("Violet are violet");
+				break;
+			}
+		}
+		else
+		{
+			printf("I don't know about the color %s.\n", choice);
+		}
+		color_is_found = false;
+		puts("Next color, please (empty line to quit():");
+	}
+	printf("Goodbye!\n");
+	getchar();
+
+	return 0;
+}
